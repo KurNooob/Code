acf1f450592bfa59c19cf70c7d913f5df4aae9a5
KurNooob <kornizusmok@gmail.com>
2023-05-08 21:16:02 +0700

    created session3

---
+++ Modul 10/Latihan/session3.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    if (isset($_SESSION['login'])) {
+        unset ($_SESSION);
+        session_destroy();
+        echo "<h1>Kamu sudah berhasil logout/keluar</h1>";
+        echo "<h1>Klik <a href='session.php'>"
+    }
+?>
