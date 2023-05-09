45bebcd46092f6a47c26060ba0aa4a0c321c0768
KurNooob <kornizusmok@gmail.com>
2023-05-09 20:26:21 +0700

    belum komplit ternyata

--- Modul 10/Latihan/session3.php
+++ Modul 10/Latihan/session3.php
@@ -4,6 +4,7 @@
         unset ($_SESSION);
         session_destroy();
         echo "<h1>Kamu sudah berhasil logout/keluar</h1>";
-        echo "<h1>Klik <a href='session.php'>";
+        echo "<h1>Klik <a href='session.php'>disini</a> untuk login lagi. <br>Kamu tidak bisa masuk ke
+        <a href='session2.php'>HOME (beranda)</a>lagi.</h2>";
     }
 ?>
