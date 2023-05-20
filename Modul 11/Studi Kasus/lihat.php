1953af496e779202d6821d5fe81a9addf1ed9bfb
KurNooob <kornizusmok@gmail.com>
2023-05-20 19:44:34 +0700

    moved to folder

---
+++ Modul 11/Studi Kasus/lihat.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Menampilkan table</title>
+</head>
+
+<body>
+    <div class="judul">
+        <h1>Detail Data</h1>
+    </div>
+    <br>
+    <br>
+
+    <?php
+    include "koneksi.php";
+    $nim = $_GET['NIM'];
+    $data = mysqli_query($koneksi, "SELECT * from data WHERE nim='$nim'") or die(mysql_error());
+    $no = 1;
+    while($d = mysqli_fetch_array($data)){
+    ?>
+        <table>
+            <tr>
+                <td>NIM</td>
+                <td>: <?php echo $d['NIM'] ?></td>
+            </tr>
+            <tr>
+                <td>Nama</td>
+                <td>: <?php echo $d['Nama'] ?></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>: <?php echo $d['Alamat'] ?></td>
+            </tr>
+            <tr></tr>
+        </table>
+        <?php } ?>
+            <a href="full.php"><<< Kembali Lihat Semua Data</a>
+</body>
+</html>
